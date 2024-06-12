618e2911c9fec992c8db2945d0407a97e3b7f411
Robert Crummett <rncrummett5@gmail.com>
2024-06-12 12:07:02 -0700

    Shell file now removes windows bat script

--- rename-linux.sh
+++ rename-linux.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
 # Rename the default writing assignment file
 #
 # Inputs [1]
 # 	the name of the new file only, no .tex necessary!
 #
 # Last edit 06-12-24
-oldname=writing-assignment.tex;
-newname=writing-assignment.tex;
-if [ -e oldname ]; then
-mv oldname newname
-echo File renamed successfully.
+oldname=writing-assignment.tex
+newname=$1.tex
+
+if [ -e $oldname ]
+then
+        mv $oldname $newname
+        echo File renamed successfully.
+
+        # Clean the current directory
+        rm rename-windows.bat
+        rm -- "$0"
 else
-echo The file does not exist.
+        echo The file does not exist.
 fi
-return
